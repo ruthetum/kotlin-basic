a2bbf7de90928b5d43caa53cdf1cb870c659ab53
Heedong Kim <ruthetum@gmail.com>
2020-08-29 23:55:00 +0900

    흐름제어 - if, when문

--- KotlinBasic/src/control/con1.kt
+++ KotlinBasic/src/control/con1.kt
@@ -2,6 +2,14 @@ package control
 
 fun main(args: Array<String>) {
     println(getBigger(1, 10))
+
+    val a = 1
+    // 코틀린에는 when 분기문이 존재 - switch와 유사
+    when (a) {
+        1 -> println("a : 1")
+        2 -> println("a : 2")
+        else -> println("a : default") // switch문에서 default 개념이 else
+    }
 }
 
 // if문이 삼항 연산자 역할을 할 수 있기 때문에
